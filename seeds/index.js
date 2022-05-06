f92bfdb4acfc8ad4a0f7bf2c0b9ada61a5816358
Obtains <97128583+Obtains@users.noreply.github.com>
2022-05-06 10:46:15 -0400

    added function and depenedencies to index

---
+++ seeds/index.js
@@ -0,0 +1,27 @@
+// Dependencies
+const seedUsers = require("./user-seeds");
+const seedPosts = require("./post-seeds");
+const seedComments = require("./comments-seeds");
+const seedVotes = require("./vote-seeds");
+
+const sequelize = require("../config/config");
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log("------------");
+    await seedUsers();
+    console.log("------------");
+
+    await seedPosts();
+    console.log("------------");
+
+    await seedComments();
+    console.log("------------");
+
+    await seedVotes();
+    console.log("------------");
+
+    process.exit(0);
+};
+
+seedAll();
