07f752db2bd09f3055fcd88d21ffc661a4b11292
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-05-16 09:49:35 -0400

    fix router

--- config/config.js
+++ config/config.js
@@ -3,25 +3,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
 });
-
-module.exports = sequelize;
-
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: ‘localhost’,
-//       dialect: ‘mysql’,
-//       port: 3306
-// });
 
 // const connection = Sequelize.createConnection({
 //     host: 'localhost',
@@ -29,8 +17,8 @@ module.exports = sequelize;
 //     password: ''
 // });
 
+module.exports = sequelize;
 
 
 
 
-//module.exports = sequelize;
